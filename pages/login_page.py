ec62dfb99d6e72fc2e75df36447f9a69d89742cd
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-03-20 10:38:12 -0300

    modified login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,29 +11,58 @@ class LoginPage(BasePage):
     _login_button = {'by': By.CSS_SELECTOR, 'value': 'button.radius'}
     _sucess_message = {'by': By.CSS_SELECTOR, 'value': 'div.flash.sucess'}
     _failure_message = {'by': By.CSS_SELECTOR, 'value': 'div.flash.error'}
-
+    _login_form = {'by': By.ID, 'value'}
 # Erro: Descoberto pelo próprio autor
 # Defeito: Descoberto de forma estática por outra pessoa
 # Falha: Descoberta de forma dinâmica por outra pessoa
 
    # 2.2 - Inicializador / Construtor (Java)
     def __init__(self, driver):
+        # instaciando o Selenium
+        self.driver = driver
+        # abrindo a pagina alvo
         self.driver.get('https://the-internet.herokuapp.com/login')
+        # validando se o formulario de login esta visivel
+        assert self.aparecer(self._login_form)
 
     def com_(self, username, password):
+        """
+        Programação Comum - Sem Page Object
+
         self.driver.find_element(self._username_input['by'],
                                  self._username_input['value']).send_keys(username)
         self.driver.find_element(self._password_input['by'],
                                  self._password_input['value']).send_keys(password)
         self.driver.find_element(self._login_button['by'],
                                  self._login_button['value']).click()
+        """
+        self._escrever(self._username_input, username)
+        self._escrever(self._password_input, password)
+        self._clicar(self._login_button)
 
    # 2.3 - Ações Realizáveis
     def vejo_mensagem_de_sucesso(self):
+        """
         return self.driver.find_element(self._sucess_message['by'],
                                  self._sucess_message['value']).is_displayed()
+        """
+        return self._aparecer(self._sucess_message, 3)
 
     def vejo_mensagem_de_falha(self):
+        """
         return self.driver.find_element(self._failure_message['by'],
                                         self._failure_message['value']).is_displayed()
+        """
+        return self._aparecer(self._failure_message, 3)
 
+    def testar_login_com_invalido(self, login):
+        # preencher o usuário, a senha e clicar no botão
+        login.com_('agjsggdsa', 'SuperSecretPassword!')
+        # validar a mensagem
+        assert login.vejo_mensagem_de_falha()
+
+    def testar_login_com_senha_invalida(self, login):
+        # preencher o usuário, a senha e clicar no botão
+        login.com_('tomsmith', 'xpto123456')
+        # validar a mensagem
+        assert login.vejo_mensagem_de_falha()
